877b77505ad87b8c84c61c6aaadb91eb6374f215
AsifOni <asif.oni@astralforminc.com>
2023-07-29 14:38:49 -0400

    Fixed styling

--- components/TwoColumnSection.jsx
+++ components/TwoColumnSection.jsx
@@ -4,26 +4,23 @@ import Image from 'next/image';
 export const TwoColumnSection = (props) => {
   const { img: masterImg, cardData } = props;
   const { src: masterSrc, alt: masterAlt } = masterImg;
+  const { id, topImg, linkText, linkUrl, ...restProps } = cardData;
+  const { src, alt } = topImg;
+
   return (
-    <div className="md:container mx-auto pad my-8" data-sb-object-id={props.id}>
-      {cardData?.map((data, idx) => {
-        const { id, topImg, linkText, linkUrl, ...restProps } = data;
-        const { src, alt } = topImg;
-        return (
-          <RewardsCard
-            key={`card-data-${idx}`}
-            topImg={src}
-            imgSRText={alt}
-            footer={
-              <a className="mt-[1.125rem]" href={linkUrl}>
-                {linkText}
-              </a>
-            }
-            {...restProps}
-          />
-        );
-      })}
-      <Image src={masterSrc} alt={masterAlt} />
+    <div className="md:container mx-auto my-8 flex justify-between" data-sb-object-id={props.id}>
+      <RewardsCard
+        style={"w-1/3"}
+        topImg={src}
+        imgSRText={alt}
+        footer={
+          <a className="mt-[1.125rem]" href={linkUrl}>
+            {linkText}
+          </a>
+        }
+        {...restProps}
+      />
+      <Image src={masterSrc} alt={masterAlt} width={520} height={412} />
     </div>
   );
 };
