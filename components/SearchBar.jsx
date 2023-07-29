97fa5848afe653cb0f93fe6bab6ae90879e58e9f
AsifOni <asif.oni@astralforminc.com>
2023-07-29 11:12:22 -0400

    Pushed Search bar code

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,6 +1,20 @@
+import { Button as RButton } from 'ccg-rewards';
+
 export const SearchBar = () => {
-    return (
-      <div className="flex">Hello man</div>
-    );
-  };
-  
+  return (
+    <div className="flex p-8 justify-center bg-caa-pink">
+      <div className="w-[625px]">
+        <div className='bg-white'>
+            <input type='text' placeholder='Search by name, keyword' aria-label='Search benefits'/>
+            <select>
+                <option></option>
+                <option></option>
+                <option></option>
+            </select>
+
+        </div>
+        <RButton label="Find Offers"/>
+      </div>
+    </div>
+  );
+};
