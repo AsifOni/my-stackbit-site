2a4b703605196f752f2b2daaec880a64fa397c0c
AsifOni <asif.oni@astralforminc.com>
2023-08-02 14:10:29 -0400

    Added component registry

--- utils/registry.js
+++ utils/registry.js
@@ -1,12 +1,14 @@
 // Custom Imports
 import { Card as RewardsCard } from '../components/Card';
 import { Hero as RewardsHero } from '../components/Hero';
+import { Button as RewardsButton } from '../components/Button';
 import { ThreeColumnCard } from '../components/ThreeColumnCard';
 import { TwoColumnSection } from '../components/TwoColumnSection';
 
 export function ComponentRegistry() {
   return {
-    rewardsHero: RewardsHero,
+    button: RewardsButton,
+    hero: RewardsHero,
     rewardsCard: RewardsCard,
     threeColumnCardSection: ThreeColumnCard,
     twoColumnSection: TwoColumnSection,
