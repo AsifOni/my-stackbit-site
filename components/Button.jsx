ed989b5c1b38abbb7a40e4e7c2665f34f1c08019
Rajdeep Chowdhuri <>
2023-08-02 11:48:52 -0400

    - feat: btn update

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,5 +2,9 @@ import { Button as RewardsButton } from 'ccg-rewards';
 
 export const Button = (props) => {
   const { label, id } = props;
-  return <RewardsButton label={label} sbDataAttr={{ label: 'label' }} data-sb-object-id={props.id} />;
+  return (
+    <div data-sb-object-id={id}>
+      <RewardsButton label={label} sbDataAttr={{ label: 'label' }} />
+    </div>
+  );
 };
